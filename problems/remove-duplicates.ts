14439e7ba67e16ddb3a7b8c76e4c285da45ad531
shivam <shivamkatyansk@gmail.com>
2023-09-10 17:11:47 +0530

    remove deplicates

---
+++ problems/remove-duplicates.ts
@@ -0,0 +1,10 @@
+function removeDuplicates(nums: number[]): number {
+    let count = 1;    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    return count;
+}
